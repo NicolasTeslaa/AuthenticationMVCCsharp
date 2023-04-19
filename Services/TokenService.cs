659e1155cdfeeb7a098fed3ed13c6c5b247009f4
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-04-19 17:39:28 -0300

    implement token

---
+++ Services/TokenService.cs
@@ -0,0 +1,37 @@
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using AuthenticationMVCCsharp.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthenticationMVCCsharp.Services
+{
+    public static class TokenService
+    {
+
+        public static string GenerateToken(User user)
+        {
+            var tokenHandler = JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, user.Username),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(TokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        private static object JwtSecurityTokenHandler()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace AuthenticationMVCCsharp
+{
+    public static class Settings
+    {
+        public static string Secret = "fedaf7db2263b48e197b9287d432b708e";
+    }
+}
