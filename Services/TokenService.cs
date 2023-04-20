45829c0b97a25bd9a833b03b65d711f3cae1c6b4
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-04-20 20:38:05 -0300

    atts

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,4 +1,4 @@
-using System.Net.Http;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using AuthenticationMVCCsharp.Models;
@@ -8,25 +8,23 @@ namespace AuthenticationMVCCsharp.Services
 {
     public static class TokenService
     {
-
         public static string GenerateToken(User user)
         {
-            var tokenHandler = JwtSecurityTokenHandler();
+            var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(Settings.Secret);
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-
                 Subject = new ClaimsIdentity(new[]
                 {
                     new Claim(ClaimTypes.Name, user.Username),
                     new Claim(ClaimTypes.Role, user.Role)
                 }),
-
                 Expires = DateTime.UtcNow.AddHours(8),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
-            var token = tokenHandler.CreateToken(TokenDescriptor);
+            var token = tokenHandler.CreateToken(tokenDescriptor);
             return tokenHandler.WriteToken(token);
+
         }
 
         private static object JwtSecurityTokenHandler()
